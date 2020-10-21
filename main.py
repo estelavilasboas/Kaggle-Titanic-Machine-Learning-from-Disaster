ee2cd2fa41e1c72556bf4c06fdfc99619675423f
estelavilasboas <estelavilasboas01@gmail.com>
2020-10-21 18:12:56 -0300

    start

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+# training set
+train_dt = pd.read_csv("./titanic/train.csv")
+# test set
+test_dt = pd.read_csv("./titanic/test.csv")
+
+train_dt.describe()
+train_dt.info()
